cb88ca702667d383738672cdb223a747f859ba6f
DenisSeroshtan <haron_777@mail.ru>
2018-07-28 00:09:41 +0500

    add method for UIctrl

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const ItemCtrl = (function(){
     
     items : [
       {id: 0, name: "Мясо", calories: 1000},
-      {id: 0, name: "Печенье", calories: 1300},
-      {id: 0, name: "Яица", calories: 700}
+      {id: 1, name: "Печенье", calories: 1300},
+      {id: 2, name: "Яица", calories: 700}
     ],
     currentItem : null,
     totalCalories : 0
@@ -24,6 +24,17 @@ const ItemCtrl = (function(){
   return {
     logData: function() {
       return data
+    },
+    addItem: function(name, calories) {
+      let id;
+      if(data.items.length !== 0) {
+        id = +(data.items.length - 1) + 1
+      } else {
+        id = 0;
+      }
+      const item = new Item(id, name, parseInt(calories));
+
+      data.items.push(item);
     },
     getItems: function() {
       return data.items
@@ -34,7 +45,11 @@ const ItemCtrl = (function(){
 //UI contoller
 const UICtrl = (function(){
   const UISelector = {
-    itemList : "#item-list"
+    itemList : "#item-list",
+    btnAdd: ".add-btn",
+    form: '#form',
+    name: "#item-name",
+    calories: "#item-calories"
   }
 
   return {
@@ -53,6 +68,15 @@ const UICtrl = (function(){
       });
 
       document.querySelector(UISelector.itemList).innerHTML = html;
+    },
+    getItemInput : function() {
+      return {
+        name: document.querySelector(UISelector.name).value,
+        calories: document.querySelector(UISelector.calories).value
+      }
+    },
+    getSelector: function() {
+      return UISelector;
     }
   }
 })();
@@ -60,11 +84,30 @@ const UICtrl = (function(){
 //App controller
 const App = (function(ItemCtrl, UICtrl){
 
+  const UIselector = UICtrl.getSelector();
+  const loadEventListener = function() {
+    document.querySelector(UIselector.form).addEventListener('submit', itemAddSubmit);
+
+  }
+
+  const itemAddSubmit = function(e) {
+    e.preventDefault();
+    const input = UICtrl.getItemInput();
+
+    if(input.name !== '' && input.calories !== '') {
+      const newItem = ItemCtrl.addItem(input.name, input.calories)
+    }
+  
+    console.table(ItemCtrl.logData().items)
+  }
+
   return {
     init: function() {
       const items = ItemCtrl.getItems();
 
       UICtrl.populateItemList(items)
+
+      loadEventListener()
     }
   }
 })(ItemCtrl, UICtrl);
